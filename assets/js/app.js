666dd9aca3fde021b3840d98fdd35778057fa6f2
Jean Gwennael <gwennael.jean@gmail.com>
2018-10-22 13:45:25 +0200

    feat(contact): affichage des contacts et du formulaires

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,11 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Fichier d'initialisation
  */
 
 $(function () {
-
-    $('.modal').on('shown.bs.modal', function () {
-        if (typeof $(this).attr('data-prototype') !== "undefined") {
-            var index = $('[data-container=' + $(this).attr('data-type-container') + '] .modal').length;
-            var form = $(this).attr('data-prototype');
-            $(this).find('.modal-body').html(form.replace(/__name__/g, index));
-        } else {
-            $(this).attr('data-form', $(this).find('.modal-body').html());
-        }
-    });
-
-    $('.modal').on('hide.bs.modal', function () {
-        if (typeof $(this).attr('data-prototype') !== "undefined") {
-            $(this).find('.modal-body').empty();
-        } else {
-            $(this).find('.modal-body').html($(this).attr('data-form'));
-        }
-    });
-});
-
     
+    $('[data-toggle="tooltip"]').tooltip();
+    
+    $('[data-toggle="popover"]').popover();
+    
+});
