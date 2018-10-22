a67399abe9359c0c996bfca2ef0a62da52c6dcd8
Jean Gwennael <gwennael.jean@gmail.com>
2018-10-22 13:43:58 +0200

    feat(contact): ajout des contacts des CV dans les fixtures

--- src/DataFixtures/CvFixture.php
+++ src/DataFixtures/CvFixture.php
@@ -3,12 +3,14 @@
 namespace App\DataFixtures;
 
 use App\DataFixtures\AbstractFixture;
+use App\DBAL\Types\ContactEnumType;
 use App\DBAL\Types\DisponibiliteEnumType;
 use App\DBAL\Types\SituationProfessionnelleEnumType;
 use App\DBAL\Types\TypeContratEnumType;
 use App\Entity\Competence;
 use App\Entity\CompetenceDomaine;
 use App\Entity\CV;
+use App\Entity\Contact;
 use App\Entity\Experience;
 use App\Entity\ExperienceInformationsGenerales;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -17,7 +19,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 class CvFixture extends AbstractFixture implements DependentFixtureInterface {
 
     const PREFIX_REFERENCE = 'cv';
-    
+
     public function load(ObjectManager $manager) {
         foreach ($this->getData() as $data) {
             $entity = new CV();
@@ -25,29 +27,36 @@ class CvFixture extends AbstractFixture implements DependentFixtureInterface {
             $entity
                     ->setNom($data['nom'])
                     ->setUser($this->getReference($this->getReferencePath(UserFixture::PREFIX_REFERENCE, $data['user_id'])));
-            
+
             if (isset($data['disponibilite'])) {
                 $entity->setDisponibilite(constant(DisponibiliteEnumType::class . '::' . $data['disponibilite']));
             }
-            
+
             if (isset($data['situation_professionnelle'])) {
                 $entity->setSituationProfessionnelle(constant(SituationProfessionnelleEnumType::class . '::' . $data['situation_professionnelle']));
             }
-            
+
+            if (isset($data['contacts'])) {
+                foreach ($data['contacts'] as $contactData) {
+                    $contact = $this->getContact($contactData);
+                    $entity->addContact($contact);
+                }
+            }
+
             if (isset($data['experiences'])) {
                 foreach ($data['experiences'] as $experienceData) {
                     $experience = $this->getExperience($experienceData);
                     $entity->addExperience($experience);
                 }
             }
-            
+
             if (isset($data['domaines_competence'])) {
                 foreach ($data['domaines_competence'] as $domainesCompetenceData) {
                     $domainesCompetence = $this->getDomaineCompetence($domainesCompetenceData);
                     $entity->addDomaineCompetence($domainesCompetence);
                 }
             }
-            
+
             $manager->persist($entity);
 
             $this->addReference($this->getReferencePath(self::PREFIX_REFERENCE, $data['id']), $entity);
@@ -55,7 +64,17 @@ class CvFixture extends AbstractFixture implements DependentFixtureInterface {
 
         $manager->flush();
     }
-    
+
+    private function getContact(array $data): Contact {
+        $contact = new Contact();
+
+        $contact
+                ->setType(constant(ContactEnumType::class . '::' . $data['type']))
+                ->setValeur($data['valeur']);
+
+        return $contact;
+    }
+
     private function getExperience(array $data): Experience {
         $experience = new Experience();
 
@@ -66,10 +85,10 @@ class CvFixture extends AbstractFixture implements DependentFixtureInterface {
                 ->setTypeContrat(constant(TypeContratEnumType::class . '::' . $data['type_contrat']))
                 ->setEntreprise($this->getReference($this->getReferencePath(EntrepriseFixture::PREFIX_REFERENCE, $data['entreprise_id'])))
                 ->setVille($this->getReference($this->getReferencePath(VilleFixture::PREFIX_REFERENCE, $data['ville_id'])));
-        
+
         return $experience;
     }
-    
+
     private function getExperienceInformationsGenerales(array $data): ExperienceInformationsGenerales {
         $informationsGenerales = new ExperienceInformationsGenerales();
 
@@ -83,33 +102,33 @@ class CvFixture extends AbstractFixture implements DependentFixtureInterface {
         } else {
             $informationsGenerales->setEnCours(TRUE);
         }
-        
+
         return $informationsGenerales;
     }
-    
+
     private function getDomaineCompetence(array $data): CompetenceDomaine {
         $domaine = new CompetenceDomaine();
-        
+
         $domaine
                 ->setNom($data['nom']);
-        
+
         if (isset($data['competences'])) {
             foreach ($data['competences'] as $competenceData) {
                 $competence = $this->getCompetence($competenceData);
                 $domaine->addCompetence($competence);
             }
         }
-        
+
         return $domaine;
     }
-    
+
     private function getCompetence(array $data): Competence {
         $competence = new Competence();
-        
+
         $competence
                 ->setNom($data['nom'])
                 ->setNote($data['note']);
-        
+
         return $competence;
     }
 
