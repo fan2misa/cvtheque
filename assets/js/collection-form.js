0bea4ce684086b7a32744c3510425c376d0840a0
Jean Gwennael <gwennael.jean@gmail.com>
2018-10-24 13:30:51 +0200

    feat(cv): suppression des expériences et domaine de compétences

--- assets/js/collection-form.js
+++ assets/js/collection-form.js
@@ -9,8 +9,6 @@ $(function () {
         var index = container.find(container.attr('data-prototype-container')).find('[data-prototype-line]').length;
         var form = container.attr('data-prototype');
 
-        console.log(index);
-
         container.find(container.attr('data-prototype-container')).append(form.replace(/__name__/g, index));
     });
 
@@ -18,4 +16,15 @@ $(function () {
         $(this).closest('[data-prototype-line]').remove();
     });
 
+    $(document).on('click', '[data-remove]', function () {
+        $(this).closest('form').find('.modal-remove').attr('data-remove-component', $(this).attr('data-remove'));
+        $(this).closest('form').find('.modal-remove').find('.modal-title').text($(this).attr('data-remove-title'));
+        $(this).closest('form').find('.modal-remove').find('.modal-body').text($(this).attr('data-remove-body'));
+        $(this).closest('form').find('.modal-remove').modal('show');
+    });
+
+    $(document).on('click', '[data-remove-confirmed]', function () {
+        $($(this).closest('.modal-remove').attr('data-remove-component')).remove();
+        $(this).closest('form').submit();
+    });
 });
