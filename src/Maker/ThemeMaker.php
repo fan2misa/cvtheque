6f8e3a84bee4e55dcd5ec2fe4f17f9d8a5a94438
Jean Gwennael <gwennael.jean@gmail.com>
2018-11-06 19:07:29 +0100

    feat(maker): ajout d'un message supplémentaire pour supprimer le fichier edition.html.twig

--- src/Maker/ThemeMaker.php
+++ src/Maker/ThemeMaker.php
@@ -83,8 +83,9 @@ class ThemeMaker extends AbstractMaker {
         $this->writeSuccessMessage($io);
 
         $io->text('Your Theme is created with name : ' . $entity->getNom());
-        $io->text('You can find template at : ' . $entity->getTemplatePath());
-        $io->text('You can find css files at : ' . $entity->getPublicPath());
+        $io->text('You can find template at : ' . $this->getTargetTemplatePath($entity->getTemplatePath()));
+        $io->text('You can find css files at : ' . $this->getTargetPublicPath($entity->getPublicPath()));
+        $io->text('If you don\'t want to edit edition part, remove file: ' . $this->getTargetTemplatePath($entity->getTemplatePathEdition()));
     }
 
     private function generateEntity(InputInterface $input, ConsoleStyle $io, Generator $generator): Theme {
