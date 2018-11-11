a6a3ea15bbcc60fd42334bbf8d71d9e6e0423430
Gwennael Jean <gwennael.jean@gmail.com>
2018-11-11 14:15:21 +0100

    feat(wrapper): création des entités Wrapper

---
+++ src/Service/Wrapper/Entity/Theme.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Service\Wrapper\Entity;
+
+class Theme {
+
+    private $templatePath;
+
+    private $publicPath;
+
+    /**
+     * @return null|string
+     */
+    public function getTemplatePath()
+    {
+        return $this->templatePath;
+    }
+
+    /**
+     * @param mixed $templatePath
+     * @return Theme
+     */
+    public function setTemplatePath($templatePath)
+    {
+        $this->templatePath = $templatePath;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getTemplatePathVisualisation(): ?string
+    {
+        return null !== $this->getTemplatePath() ? $this->getTemplatePath() . '/visualisation.html.twig' : null;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getPublicPath()
+    {
+        return $this->publicPath;
+    }
+
+    /**
+     * @param mixed $publicPath
+     * @return Theme
+     */
+    public function setPublicPath($publicPath)
+    {
+        $this->publicPath = $publicPath;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getCssPathGlobal(): ?string
+    {
+        return null !== $this->getPublicPath() ? $this->getPublicPath() . '/css/theme.css' : null;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getCssPathEdition(): ?string
+    {
+        return null !== $this->getPublicPath() ? $this->getPublicPath() . '/css/theme-edition.css' : null;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getCssPathVisualisation(): ?string
+    {
+        return null !== $this->getPublicPath() ? $this->getPublicPath() . '/css/theme-visualisation.css' : null;
+    }
+
+}
