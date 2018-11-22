ec8f1549ebfc88edc808b72d073f67388337b30d
Gwennael Jean <gwennael.jean@gmail.com>
2018-11-22 20:54:45 +0100

    feat(cv): création d'un CV avec thème standard

--- src/Controller/Cv/AjouterController.php
+++ src/Controller/Cv/AjouterController.php
@@ -4,17 +4,19 @@ namespace App\Controller\Cv;
 
 use App\Entity\Cv;
 use App\Form\CVType;
+use App\Service\CVService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
 class AjouterController extends AbstractController {
 
-    public function index(Request $request) {
-        $cv = new Cv();
+    public function index(CVService $cvService, Request $request) {
+        $cv = $cvService->create();
 
         $form = $this->createForm(CVType::class, $cv);
 
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             
         }
